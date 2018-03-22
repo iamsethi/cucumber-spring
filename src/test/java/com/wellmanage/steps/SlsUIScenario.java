f92d2c556ae407cc634da59a71a0c534578353fb
kseth9 <kseth9@dlukseth912450.sapient.com>
2018-03-22 13:51:12 +0530

    test

--- src/test/java/com/wellmanage/steps/SlsUIScenario.java
+++ src/test/java/com/wellmanage/steps/SlsUIScenario.java
@@ -1,5 +1,60 @@
 package com.wellmanage.steps;
 
-public class SlsUIScenario {
+import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
+import org.springframework.test.context.BootstrapWith;
+import org.springframework.test.context.ContextConfiguration;
+
+import cucumber.api.java8.En;
+
+@BootstrapWith(SpringBootTestContextBootstrapper.class)
+@ContextConfiguration(locations = { "classpath*:/spring-context/applicationContext-smoke-suite.xml" })
+
+public class SlsUIScenario implements En {
+	public SlsUIScenario() {
+		Given("^I want to write a step with precondition$", () -> {
+
+		});
+
+		Given("^some other precondition$", () -> {
+
+		});
+
+		When("^I complete action$", () -> {
+
+		});
+
+		When("^some other action$", () -> {
+
+		});
+
+		When("^yet another action$", () -> {
+
+		});
+
+		Then("^I validate the outcomes$", () -> {
+
+		});
+
+		Then("^check more outcomes$", () -> {
+
+		});
+
+		Given("^I want to write a step with name(\\d+)$", (Integer arg1) -> {
+
+		});
+
+		When("^I check for the (\\d+) in step$", (Integer arg1) -> {
+
+		});
+
+		Then("^I verify the success in step$", () -> {
+
+		});
+
+		Then("^I verify the Fail in step$", () -> {
+
+		});
+
+	}
 
 }
