e2eee614e6ecc576a8c5d1cd277cea33d0d21495
iamsethi <ketansethi786@gmail.com>
2018-03-27 10:53:09 +0530

    Update Constants.java

--- src/main/java/com/wellmanage/database/Constants.java
+++ src/main/java/com/wellmanage/database/Constants.java
@@ -1,8 +1,29 @@
 package com.wellmanage.database;
 
+/**
+ * The Interface Constants.
+ */
 public interface Constants {
 
-	/** the loc code. */
-	String LOC_CODE = "LOC_CODE";
+	/** The trade letter id. */
+	String TRADE_LETTER_ID = "tradeLetterId";
+
+	/** The file id. */
+	String FILE_ID = "fileId";
+
+	/** The admin id. */
+	String ADMIN_ID = "adminId";
+
+	/** The admin party id. */
+	String ADMIN_PARTY_ID = "adminPartyId";
+
+	/** The party id. */
+	String PARTY_ID = "partyId";
+
+	/** The address id. */
+	String ADDRESS_ID = "addressId";
+
+	/** The contact id. */
+	String CONTACT_ID = "contactId";
 
 }
